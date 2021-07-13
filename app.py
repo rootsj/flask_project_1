c975162751c512f72ab2d24c7e0becd28c5edf29
rootsj <hwa9981@naver.com>
2021-07-13 20:26:54 +0900

    json test2

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient  
 
-# from bson.objectid import ObjectId
+from bson import json_util
+from mongoengine_jsonencoder import MongoJSONEncoder
+# , ObjectIdConverter
+
+from bson.objectid import ObjectId
+#str -> ObjectId
 
 app = Flask(__name__)
+app.json_encoder = MongoJSONEncoder
 
 client = MongoClient('localhost', 27017) 
 db = client.dbsparta  
@@ -27,27 +33,37 @@ def post_article():
 
 @app.route('/memo/list', methods=['GET'])
 def read_articles():
-   result = list(db.articles.find({}, {'_id': 0})) 
-   #2 _id -> string 변경 후 json 전달 필요
+   # result = list(db.articles.find({}, {'_id': 0})) 
+   result = list(db.articles.find({})) 
+
    return jsonify({'result': 'success', 'articles': result})
 
 
-@app.route('/memo/check', methods=['POST'])
-def check_article():
-   title_receive = request.form['title_give']
+# @app.route('/memo/check', methods=['POST'])
+# def check_article():
+#    title_receive = request.form['title_give']
 
-   article = db.articles.find_one({'title': title_receive})
-   global id_receive #3 임시 전역변수. read_article에서 pk _id 넘기고 삭제 예정
-   id_receive = article['_id']
+#    article = db.articles.find_one({'title': title_receive})
+#    global id_receive #3 임시 전역변수. read_article에서 pk _id 넘기고 삭제 예정
+#    id_receive = article['_id']
 
-   return jsonify({'result': 'success'})
+#    return jsonify({'result': 'success'})
 
 @app.route('/memo/modify', methods=['POST'])
 def modify_article():
+   id_receive = request.form['id_give']
    title_receive = request.form['title_give']
    comment_receive = request.form['comment_give']
 
-   db.articles.update({'_id': id_receive}, {'$set': {'title': title_receive, 'comment': comment_receive}})
+   # print(type(id_receive))
+   # print(id_receive)
+   #2 str -> objectID 변경 필요
+   bson_id = ObjectId(id_receive)
+
+   print(bson_id)
+   print(type(bson_id))
+
+   db.articles.update_one({'_id': bson_id}, {'$set': {'title': title_receive, 'comment': comment_receive}})
 
    return jsonify({'result': 'success'})
 
